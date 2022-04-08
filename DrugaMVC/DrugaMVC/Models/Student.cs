ebba89094317ace9151af0f4a4e18c7663519107
Zan Zidaric <zan.502@gmail.com>
2022-04-08 10:08:41 +0200

    last commit 8.4.2022

--- DrugaMVC/DrugaMVC/Models/Student.cs
+++ DrugaMVC/DrugaMVC/Models/Student.cs
@@ -11,8 +11,10 @@ namespace DrugaMVC.Models
         //prop tab tab (kratica)
         public int StudentId { get; set; }
         [Display(Name ="Ime")]
+        [Required(ErrorMessage ="Ime je obvezen podatek!")]
         public string StudentName { get; set; }
         [Display(Name ="Starost")]
+        [Range(5,50,ErrorMessage ="Starost more biti od 5 do 50")]
         public int Age { get; set; }
         
     }
